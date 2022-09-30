bab36c0e2308503422004298a3a88fabc2aa1d03
trustpizza <maxelolsson@gmail.com>
2022-09-30 18:38:25 -0500

    Add method to determine which players turn it is

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,19 +3,28 @@ require_relative 'board.rb'
 require_relative 'symbols.rb'
 
 class ConnectFour
-    attr_accessor :board, :p1, :p2
-    attr_reader :round
+    attr_accessor :board, :p1, :p2, :rounds_played
+
     include Symbols
 
     def initialize
         @board = Board.new
         @p1 = Player.new(red_circle)
         @p2 = Player.new(yellow_circle)
+        @rounds_played = 0
+        play()
     end
 
     def play
         system("clear")
         show_board()
+        play_round()
+    end
+
+    def play_round
+        player = p1 if @rounds_played.even?
+        player = p2 if @rounds_played.odd?
+        p player        
     end
 
     def show_board
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -13,6 +13,19 @@ describe ConnectFour do
             game = ConnectFour.new
             expect(game.p2.piece).to eql("\e[0;33;49m●\e[0m")
         end
+
+        it "Rounds played starts at 0" do
+            game = ConnectFour.new
+            expect(game.rounds_played).to eql(0)
+        end
+    end
+
+    describe "#Play" do 
+        it "Round 1 begins with Player 1's turn" do
+            game = ConnectFour.new
+            p1 = game.p1
+            expect(game.play_round).to eql(p1)
+        end
     end
 end
 
